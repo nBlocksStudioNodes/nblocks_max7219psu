50df8e00889bf1fef2a2b4a6cd0a44f7e38f2e03
nikos chalikias <nikos.chalikias@gmail.com>
2020-11-19 19:34:17 +0000

    tested and working

--- max7219psu.cpp
+++ max7219psu.cpp
@@ -4,51 +4,61 @@ nBlock_MAX7219PSU::nBlock_MAX7219PSU(PinName MOSI, PinName MISO, PinName SCK, Pi
     _spi(MOSI, MISO, SCK), _cs(pinCS)  {
     _spi.format(8,0);                // 8-bit format, mode 0,0
     _spi.frequency(1000000);         // SCLK = 1 MHz
-    
+    _cs = 1;
+
 	// Reset flag
 	must_update = 0;
-    init_MAX7219();		
+    init_MAX7219(Brightness, ScanLimit);		
 }
 
 void nBlock_MAX7219PSU::triggerInput(nBlocks_Message message){
 	if (message.inputNumber == 0) {
-        Value = message.intValue;       // Get Value from the 1st Input		
-		Position = 1;
-        must_update =  1;               // Set flag so we update the MAX7219 at the end of frame
+        Value1 = message.intValue;       // Get Value from the 1st Input		
+		Position1 = 1;
 	}
     if (message.inputNumber == 1) {
-        Value = message.intValue;       // Get Value from the 2nd Input		
-		Position = 2;
-        must_update =  1;               // Set flag so we update the MAX7219 at the end of frame
+        Value2 = message.intValue;       // Get Value from the 2nd Input		
+		Position2 = 1;
     }
 }
 
 void nBlock_MAX7219PSU::endFrame(void){
-	if (must_update) {
-		must_update = 0;
-		max7219_write(Value, Position, Brightness, ScanLimit);
+	if (Position1) {
+		Position1 = 0;
+		max7219_write1(Value1);
+    }
+	if (Position2) {
+		Position2 = 0;
+		max7219_write2(Value2);
 	}	
 }
 
-void nBlock_MAX7219PSU::max7219_write(uint16_t Value, uint16_t Position, uint16_t Brightness, uint16_t ScanLimit) {
-    _cs = 1;                          // CS initially High 
-    spi_write_2bytes(0x0A, Brightness); 
-    spi_write_2bytes(0x0B, ScanLimit); 
-
-    // Encode Value and Position to digits
-    if (Position == 1) offset = 4;      // This is for Volts
-    if (Position == 2) offset = 0;      // This is for Amperes
-    dig[3 + offset] = (Value / 1000);
-    dig[2 + offset] = (Value - dig[3 + offset] * 1000) / 100;
-    dig[1 + offset] = (Value - dig[3 + offset] * 1000 - dig[2 + offset] * 100) / 10;
-    dig[0 + offset] = (Value - dig[3 + offset] * 1000 - dig[2 + offset] * 100 - dig[1 + offset] * 10);
-
+void nBlock_MAX7219PSU::max7219_write1(uint16_t Value1) {
+    _cs = 1;                                // CS initially High 
+    dig[3] = (Value1 / 1000);               // Encode Value and Position to digits
+    dig[2] = (Value1 - dig[3] * 1000) / 100;
+    dig[1] = (Value1 - dig[3] * 1000 - dig[2] * 100) / 10;
+    dig[0] = (Value1 - dig[3] * 1000 - dig[2] * 100 - dig[1] * 10);
     // Send digit values to MAX7219
-    spi_write_2bytes(0x01 + offset, dig[0 + offset]);     //digit 0 
-    spi_write_2bytes(0x02 + offset, dig[1 + offset]);     //digit 1 
-    spi_write_2bytes(0x03 + offset, 0x80 + dig[2 + offset]);     //digit 2 has active the decimal point
-    spi_write_2bytes(0x04 + offset, dig[3 + offset]);     //digit 3 
+    spi_write_2bytes(0x01, dig[3]);         //digit 0 
+    spi_write_2bytes(0x02, dig[2]);         //digit 1 
+    spi_write_2bytes(0x03, dig[1]);         //digit 2 has active the decimal point
+    spi_write_2bytes(0x04, dig[0]);         //digit 3 
 }
+
+void nBlock_MAX7219PSU::max7219_write2(uint16_t Value2) {
+    _cs = 1;                                // CS initially High 
+    dig[7] = (Value2 / 1000);               // Encode Value and Position to digits
+    dig[6] = (Value2 - dig[7] * 1000) / 100;
+    dig[5] = (Value2 - dig[7] * 1000 - dig[6] * 100) / 10;
+    dig[4] = (Value2 - dig[7] * 1000 - dig[6] * 100 - dig[5] * 10);
+    // Send digit values to MAX7219
+    spi_write_2bytes(0x05, dig[7]);         //digit 0 
+    spi_write_2bytes(0x06, dig[6]);         //digit 1 
+    spi_write_2bytes(0x07, dig[5]);         //digit 2 has active the decimal point
+    spi_write_2bytes(0x08, dig[4]);         //digit 3 
+}
+
 
 void nBlock_MAX7219PSU::spi_write_2bytes(unsigned char MSB, unsigned char LSB) {
     _cs = 0;                         // Set CS Low
@@ -57,24 +67,24 @@ void nBlock_MAX7219PSU::spi_write_2bytes(unsigned char MSB, unsigned char LSB) {
     _cs = 1;                         // Set CS High
 }
 
-void nBlock_MAX7219PSU::init_MAX7219(void) {
+void nBlock_MAX7219PSU::init_MAX7219(uint16_t Brightness, uint16_t ScanLimit) {
     _cs = 1;                               // CS initially High 
     spi_write_2bytes(0x09, 0xFF);         // Decoding off //nikos changed to full decode
-    spi_write_2bytes(0x0A, 0x08);         // Brightness to intermediate
-    spi_write_2bytes(0x0B, 0x07);         // Scan limit 7th digit
+    spi_write_2bytes(0x0A, Brightness);         // Brightness to intermediate
+    spi_write_2bytes(0x0B, ScanLimit);         // Scan limit 7th digit
     spi_write_2bytes(0x0C, 0x01);         // Normal operation mode, this is the shutdown register
     spi_write_2bytes(0x0F, 0x0F);         // Enable display test
 
-    wait_us(500000);                   // 500 ms delay
+    //wait_us(500000);                   // 500 ms delay
 
-    spi_write_2bytes(0x01, 0x00);         // Clear row 0.
-    spi_write_2bytes(0x02, 0x00);         // Clear row 1.
-    spi_write_2bytes(0x03, 0x00);         // Clear row 2.
-    spi_write_2bytes(0x04, 0x00);         // Clear row 3.
-    spi_write_2bytes(0x05, 0x00);         // Clear row 4.
-    spi_write_2bytes(0x06, 0x00);         // Clear row 5.
-    spi_write_2bytes(0x07, 0x00);         // Clear row 6.
-    spi_write_2bytes(0x08, 0x00);         // Clear row 7.
-    spi_write_2bytes(0x0F, 0x00);         // Disable display test
+    spi_write_2bytes(0x01, 0x0F);         // Clear row 0.
+    spi_write_2bytes(0x02, 0x0F);         // Clear row 1.
+    spi_write_2bytes(0x03, 0x0F);         // Clear row 2.
+    spi_write_2bytes(0x04, 0x0F);         // Clear row 3.
+    spi_write_2bytes(0x05, 0x0F);         // Clear row 4.
+    spi_write_2bytes(0x06, 0x0F);         // Clear row 5.
+    spi_write_2bytes(0x07, 0x0F);         // Clear row 6.
+    spi_write_2bytes(0x08, 0x0F);         // Clear row 7.
+    spi_write_2bytes(0x0F, 0x08);         // Disable display test
 
 }
--- max7219psu.h
+++ max7219psu.h
@@ -8,21 +8,23 @@ public:
     nBlock_MAX7219PSU(PinName MOSI, PinName MISO, PinName SCK, PinName pinCS, uint16_t Brightness, uint16_t ScanLimit);
     void triggerInput(nBlocks_Message message);
 	void endFrame();
-	void max7219_write(uint16_t Value, uint16_t Position, uint16_t Brightness, uint16_t ScanLimit);
+	void max7219_write1(uint16_t Value1);
+    void max7219_write2(uint16_t Value2);
     void spi_write_2bytes(unsigned char MSB, unsigned char LSB);
-    void init_MAX7219(void);
+    void init_MAX7219(uint16_t Brightness, uint16_t ScanLimit);
 
    
 private:	
     SPI _spi;
     DigitalOut _cs;
-	uint32_t Value;
-    uint32_t Position;
+	uint32_t Value1;
+    uint32_t Value2;
+    uint32_t Position1;
+    uint32_t Position2;
 	uint32_t must_update = 0;
     uint16_t Brightness; 
     uint16_t ScanLimit;
     uint16_t dig[8];
-    uint16_t offset;
 };
 
 #endif
